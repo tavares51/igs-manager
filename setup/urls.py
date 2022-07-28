e3ace60c4613155264b7806b483dca46bee41861
Victor Tavares <vh.st1503@outlook.com>
2022-07-28 16:52:30 -0300

    commit

--- setup/urls.py
+++ setup/urls.py
@@ -24,7 +24,7 @@ from drf_yasg.views import get_schema_view as swagger_get_schema_view
 
 schema_view = swagger_get_schema_view(
     openapi.Info(
-        title="Posts API",
+        title="API IGS Manager",
         default_version='1.0.0',
         description="API documentation of App",
     ),
